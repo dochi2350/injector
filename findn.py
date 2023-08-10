6221b1857e88461ebedc36ce9987b71d8ec732b5
dochi2350 <222217@dg1s.hs.kr>
2023-08-10 21:38:05 +0900

    Add major files

--- findn.py
+++ findn.py
@@ -1,7 +1,7 @@
 with open('temp.txt', 'r') as file:
     lines = file.readlines()
 
-lines = [line for line in lines if len(line.strip().split()) == 4]
+lines = [line for line in lines if len(line.strip().split()) == 8]
 
 for line in lines:
     print(line, end = "")
